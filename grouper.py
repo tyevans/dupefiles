495f0d0f98f82712c0ac12b243a935aa18c7ff26
Tyler Evans <tyevans@gmail.com>
2017-12-14 12:02:03 -0800

    Initial Commit

---
+++ grouper.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+import argparse
+import collections
+import hashlib
+import os
+import fnmatch
+
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+DEFAULT_MIN_SIZE = 0
+DEFAULT_MAX_SIZE = 1000000000
+
+
+def options():
+    parser = argparse.ArgumentParser(description='Search for duplicated files across a directory structure.')
+    parser.add_argument('--min-size', type=int, default=DEFAULT_MIN_SIZE,
+                        help='Minimum file size in bytes (files smaller than this will be skipped)')
+    parser.add_argument('--max-size', type=int, default=DEFAULT_MAX_SIZE,
+                        help='Maximum file size in bytes (files larger than this will be skipped)')
+    parser.add_argument('--name', default=None,
+                        help='Filename glob. Only files matching this pattern will be considered.')
+    parser.add_argument('--followlinks', action="store_true", default=False,
+                        help='Follow symlinks')
+    parser.add_argument('path', type=str, help='The directory path to search')
+    return parser.parse_args()
+
+
+def walk(path, glob=None, followlinks=False):
+    try:
+        path_iter = os.scandir(path)
+    except OSError as exc:
+        logger.warning(f"Skipping Directory (OSError): {path}")
+        return
+
+    walk_dirs = []
+    with path_iter:
+        for entry in path_iter:
+            if entry.is_dir():
+                if followlinks:
+                    walk_into = True
+                else:
+                    try:
+                        is_symlink = entry.is_symlink()
+                    except OSError:
+                        # If is_symlink() raises an OSError, consider that the
+                        # entry is not a symbolic link, same behaviour than
+                        # os.path.islink().
+                        is_symlink = False
+                    walk_into = not is_symlink
+
+                if walk_into:
+                    walk_dirs.append(entry.path)
+            if entry.is_file() and (glob is None or fnmatch.fnmatch(entry.name, glob)):
+                yield entry
+
+    for directory in walk_dirs:
+        for entry in walk(directory, glob):
+            yield entry
+
+
+def group_by_key(fileset, keyfunc, min_group_size=2):
+    files_by_key = {}
+    for entry in fileset:
+        key = keyfunc(entry)
+        if key is None:
+            continue
+        files_by_key.setdefault(key, []).append(entry)
+    return [group for group in files_by_key.values() if len(group) >= min_group_size]
+
+
+def group_by_hash(fileset, hashfunc=hashlib.md5, min_group_size=2):
+    def key(_file):
+        try:
+            with open(_file.path, 'rb') as fd:
+                # For particularly large files we should read in chunks and update the hash function.
+                # For now lets just do it all in one shot.
+                return hashfunc(fd.read()).digest()
+        except OSError:
+            logger.warning(f"Skipping File (Could Not Open To Read): {_file.path}")
+
+    return group_by_key(fileset, key, min_group_size=min_group_size)
+
+
+def group_by_size(fileset, min_size=DEFAULT_MIN_SIZE, max_size=DEFAULT_MAX_SIZE, min_group_size=2):
+    def key(_file):
+        size = _file.stat().st_size
+        if min_size <= size <= max_size:
+            return size
+
+    return group_by_key(fileset, key, min_group_size=min_group_size)
+
+
+def find_dupe_files(path, glob=None, min_size=DEFAULT_MIN_SIZE, max_size=DEFAULT_MAX_SIZE, min_group_size=2):
+    return [group_by_hash(group, min_group_size=min_group_size) for group in
+            group_by_size(walk(path, glob), min_size=min_size, max_size=max_size, min_group_size=min_group_size)]
+
+
+if __name__ == "__main__":
+    args = options()
+    for group in find_dupe_files(args.path, args.name, args.min_size, args.max_size):
+        for entry in group:
+            print(entry.path)
+        print()
---
+++ test_finddupes.py
@@ -0,0 +1,92 @@
+from unittest import TestCase
+
+from finddupes import find_dupe_files, group_by_size, walk, group_by_hash
+
+# def group_by_size(fileset, min_size=DEFAULT_MIN_SIZE, max_size=DEFAULT_MAX_SIZE):
+#     files_by_size = {}
+#     for entry in fileset:
+#         size = entry.stat().st_size
+#         if min_size <= size <= max_size:
+#             files_by_size.setdefault(size, []).append(entry)
+#     return [group for group in files_by_size.values() if len(group) > 1]
+
+TEST_DATA_DIR = './test_data'
+
+
+class TestGroupBySize(TestCase):
+
+    def test_group_files(self):
+        file_iter = walk(TEST_DATA_DIR, "*.txt")
+        groups = group_by_size(file_iter)
+        self.assertEqual(3, len(groups))
+        for group in groups:
+            self.assertTrue(group[0].name in ['12bytes.txt', '8bytes.txt', 'empty.txt'])
+            self.assertEqual(4, len(group))
+
+    def test_group_size_1(self):
+        file_iter = walk(TEST_DATA_DIR)
+        groups = group_by_size(file_iter, min_group_size=1)
+        self.assertEqual(4, len(groups))
+        for group in groups:
+            if group[0].name in ['12bytes.txt', '8bytes.txt', 'empty.txt']:
+                self.assertEqual(4, len(group))
+            elif group[0].name == 'unique.gif':
+                self.assertEqual(1, len(group))
+
+
+class TestGroupByHash(TestCase):
+
+    def test_group_files(self):
+        file_iter = walk(TEST_DATA_DIR, "*.txt")
+        groups = group_by_hash(file_iter)
+        self.assertEqual(3, len(groups))
+        for group in groups:
+            self.assertTrue(group[0].name in ['12bytes.txt', '8bytes.txt', 'empty.txt'])
+            self.assertEqual(4, len(group))
+
+
+class TestFindDupes(TestCase):
+
+    def test_filter_empty_files(self):
+        groups = find_dupe_files(TEST_DATA_DIR, min_size=1)
+        for group in groups:
+            self.assertFalse(group[0].name == "empty.txt")
+
+    def test_only_empty_files(self):
+        groups = find_dupe_files(TEST_DATA_DIR, max_size=0)
+        self.assertEqual(1, len(groups))
+        self.assertEqual(4, len(groups[0]))
+        self.assertEqual("empty.txt", groups[0][0].name)
+
+    def test_files_smaller_than_8_bytes(self):
+        groups = find_dupe_files(TEST_DATA_DIR, max_size=7)
+        self.assertEqual(1, len(groups))
+        self.assertEqual(4, len(groups[0]))
+        self.assertEqual("empty.txt", groups[0][0].name)
+
+    def test_files_larger_than_8_bytes(self):
+        groups = find_dupe_files(TEST_DATA_DIR, min_size=9)
+        self.assertEqual(len(groups), 1)
+        self.assertEqual(len(groups[0]), 4)
+        self.assertEqual(groups[0][0].name, "12bytes.txt")
+
+    def test_files_8_bytes_or_larger(self):
+        groups = find_dupe_files(TEST_DATA_DIR, min_size=8)
+        self.assertEqual(len(groups), 2)
+        for group in groups:
+            self.assertEqual(len(group), 4)
+            self.assertTrue(group[0].name in ["8bytes.txt", "12bytes.txt"])
+
+    def test_glob(self):
+        groups = find_dupe_files(TEST_DATA_DIR, glob='*.gif', min_group_size=1)
+        self.assertEqual(len(groups), 1)
+        for group in groups:
+            self.assertEqual(len(group), 1)
+            self.assertEqual(group[0].name, 'unique.gif')
+
+    def test_glob_and_size(self):
+        groups = find_dupe_files(TEST_DATA_DIR, glob='*.txt', min_size=7, max_size=9)
+        self.assertEqual(len(groups), 1)
+        for group in groups:
+            self.assertEqual(len(group), 4)
+            self.assertEqual(group[0].name, '8bytes.txt')
